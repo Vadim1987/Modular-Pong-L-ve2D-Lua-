7da9ac9e41f4e26b46374954b444b2363871a6e4
Vadim1987 <vadim.stetskiy@gmail.com>
2025-06-18 01:06:37 +0200

    Update constants.lua

--- constants.lua
+++ constants.lua
@@ -1,4 +1,5 @@
 -- constants.lua
+
 local C = {}
 
 C.WINDOW_WIDTH     = 800
@@ -19,19 +20,17 @@ C.SCORE_FONT_SIZE  = 40
 C.SCORE_TO_WIN     = 1
 
 -- Player 1 (left paddle) controls
-KEY_UP   = 'q'
-KEY_DOWN = 'a'
+C.KEY_UP   = 'w'      
+C.KEY_DOWN = 's'      
 
 -- Player 2 (right paddle) controls for manual opponent mode
-KEY_RIGHT_UP   = 'up'
-KEY_RIGHT_DOWN = 'down'
+C.KEY_RIGHT_UP   = 'up'
+C.KEY_RIGHT_DOWN = 'down'
 
 -- Toggle: set to true for two-player mode (manual right paddle)
-MANUAL_OPPONENT = true
+C.MANUAL_OPPONENT = true
 
 -- Ball properties
-BALL_RADIUS = BALL_SIZE -- ← instead of 1x1 square, we now use radius
+C.BALL_RADIUS = C.BALL_SIZE 
 
-return c
-
-
+return C
